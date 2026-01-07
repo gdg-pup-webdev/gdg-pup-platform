53c556c081304c08c6814689284159e3b1a929be
SauceCode01 <sauce.code.01@gmail.com>
2026-01-08 02:03:07 +0800

    deug

--- apps/identity-api/src/loaders/routes.loader.ts
+++ apps/identity-api/src/loaders/routes.loader.ts
@@ -1,5 +1,8 @@
 import { Express } from "express";
 import { healthCheckRoute } from "@/modules/healthCheck/healthCheck.route.js";
 export const routesLoader = (app: Express) => {
+  app.use("/", (req, res) => {
+    res.send("Welcome to Identity API");
+  });
   app.use("/api/health", healthCheckRoute.getRouter());
 };
