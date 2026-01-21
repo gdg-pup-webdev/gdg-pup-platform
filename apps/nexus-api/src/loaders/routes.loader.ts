b1afe63aabd4395e6d79b80ef047ef4ae53d46f2
SauceCode01 <sauce.code.01@gmail.com>
2026-01-21 15:18:30 +0800

    economy sys

--- apps/nexus-api/src/loaders/routes.loader.ts
+++ apps/nexus-api/src/loaders/routes.loader.ts
@@ -10,6 +10,7 @@ import { rbacSystemRouterInstance } from "@/modules/rbacSystem/rbacSystem.router
 import { rewardSystemRouterInstance } from "@/modules/rewardsSystem/rewardSystem.route.js";
 import { teamSystemRouterInstance } from "@/modules/teamsSystem/teamSystem.router.js"; 
 import { userResourceSystemRouter } from "@/modules/userResourceSystem/index.js";
+import { economySystemRouterInstance } from "@/modules/economySystem/index.js";
 
 export const routesLoader = (app: Express) => {
   app.use("/api/health", healthCheckRouterInstance.getRouter());
@@ -45,6 +46,8 @@ export const routesLoader = (app: Express) => {
     "/api/user-resource-system",
     userResourceSystemRouter.getRouter(),
   );
+
+  app.use("/api/economy-system", economySystemRouterInstance.getRouter());
 
   app.use("/api/user-system", userSystemRouterInstance.getRouter());
 };
