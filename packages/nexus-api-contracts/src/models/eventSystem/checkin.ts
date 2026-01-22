9c7abc3007efb3c5936757d59818b849a3e0bf57
SauceCode01 <sauce.code.01@gmail.com>
2026-01-22 18:36:16 +0800

    feat: added enum contraints on checkin method in events

--- packages/nexus-api-contracts/src/models/eventSystem/checkin.ts
+++ packages/nexus-api-contracts/src/models/eventSystem/checkin.ts
@@ -4,5 +4,5 @@ import z from "zod";
 export const insertDTO = z.object({
   eventId: z.string(),
   attendeeId: z.string(),
-  checkinMethod: z.string(),
+  checkinMethod: z.enum(["QR_CODE", "MANUAL", "GOOGLE_FORMS", "NFC"]),
 });
