8d4c17b3b9953780b19dba3778ca0523e249683c
Randall Graida <carlosrandallgraida@gmail.com>
2026-01-23 16:32:52 +0800

    feat(backend): add index files

--- apps/nexus-api/src/modules/userResourceSystem/index.ts
+++ apps/nexus-api/src/modules/userResourceSystem/index.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { ProfileRouter, profileRouterInstance } from "./profile.router";
-import { projectRotuerInstance, ProjectRouter } from "./project.route";
+import { ProfileRouter, profileRouterInstance } from "./profile.router.js";
+import { projectRotuerInstance, ProjectRouter } from "./project.route.js";
+import { achievementRouterInstance, AchievementRouter } from "./achievement.route.js";
+import { certificateRouterInstance, CertificateRouter } from "./certificate.route.js";
+import { settingsRouterInstance, SettingsRouter } from "./settings.route.js";
 
 export class UserResourceSystemRouter {
   constructor(
     private profileRouter: ProfileRouter = profileRouterInstance,
     private projectRouter: ProjectRouter = projectRotuerInstance,
+    private achievementRouter: AchievementRouter = achievementRouterInstance,
+    private certificateRouter: CertificateRouter = certificateRouterInstance,
+    private settingsRouter: SettingsRouter = settingsRouterInstance,
   ) {}
 
   getRouter = (): Router => {
@@ -13,6 +19,9 @@ export class UserResourceSystemRouter {
 
     router.use("/profiles", this.profileRouter.getRouter());
     router.use("/projects", this.projectRouter.getRouter());
+    router.use("/achievements", this.achievementRouter.getRouter());
+    router.use("/certificates", this.certificateRouter.getRouter());
+    router.use("/settings", this.settingsRouter.getRouter());
 
     return router;
   };
--- packages/nexus-api-contracts/src/models/userResourceSystem/index.ts
+++ packages/nexus-api-contracts/src/models/userResourceSystem/index.ts
@@ -1,1 +1,4 @@
 export * as project from "./project.js";
+export * as achievement from "./achievement.js";
+export * as certificate from "./certificate.js";
+export * as settings from "./settings.js";
