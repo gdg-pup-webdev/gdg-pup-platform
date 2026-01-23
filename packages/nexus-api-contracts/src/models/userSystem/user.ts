84e54c8f62d8257a9070339d0a00f25e763e633d
Randall Graida <carlosrandallgraida@gmail.com>
2026-01-23 16:31:09 +0800

    refactor(schema): add zod in user.ts for type safety

--- packages/nexus-api-contracts/src/models/userSystem/user.ts
+++ packages/nexus-api-contracts/src/models/userSystem/user.ts
@@ -5,17 +5,35 @@ import {
 } from "#types/supabase.schema.js";
 import { z } from "zod";
 import { row as profileRow } from "./profile.js";
-import { project } from "../userResourceSystem";
+import {
+  achievement,
+  certificate,
+  project,
+  settings,
+} from "../userResourceSystem";
 import { wallet } from "../economySystem";
 
 export const row = publicUserRowSchema;
 
-export const insertDTO = publicUserInsertSchema;
+export const insertDTO = publicUserInsertSchema.extend({
+  email: z.string().email(),
+  display_name: z.string().min(2).max(50),
+  first_name: z.string().optional().nullable(),
+  last_name: z.string().optional().nullable(),
+  avatar_url: z.string().url().optional().nullable(),
+});
 
-export const updateDTO = publicUserUpdateSchema;
+export const updateDTO = publicUserUpdateSchema.extend({
+  email: z.string().email().optional(),
+  display_name: z.string().min(2).max(50).optional(),
+  avatar_url: z.string().url().optional().nullable(),
+});
 
 export const aggregate = row.extend({
   profiles: profileRow.array(),
   projects: project.row.array(),
   wallets: wallet.row.array(),
+  achievements: achievement.row.array(),
+  certificates: certificate.row.array(),
+  settings: settings.row.array(),
 });
