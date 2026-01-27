200e337a147c6e02c78ded5c871722d1564dbd29
Randall Graida <carlosrandallgraida@gmail.com>
2026-01-27 15:46:02 +0800

    refactor(structure): align files to feature domain folder structure

--- apps/nexus-api/src/modules/rewardsSystem/reward.utils.ts
+++
@@ -1,4 +0,0 @@
-import { customAlphabet } from "nanoid";
-
-export const generateCouponCode = (length: number = 6): string =>
-  customAlphabet("23456789abcdefghjkmnpqrstxyz", length)(); // 1,l,i, & 0, o & u, v, w are excluded to avoid confusion
