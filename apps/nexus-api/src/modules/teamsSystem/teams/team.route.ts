03efe0572bfa766ed25d2f067fa652f4dc717898
Randall Graida <carlosrandallgraida@gmail.com>
2026-01-27 16:15:04 +0800

    refactor(structure): organize teamSystem folders and files to align with feature domain folder structure

---
+++ apps/nexus-api/src/modules/teamsSystem/teams/team.route.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import { TeamController, teamControllerInstance } from "./team.controller.js";
+import {
+  AuthMiddleware,
+  authMiddlewareInstance,
+} from "@/middlewares/auth.middleware.js";
+
+export class TeamRouter {
+  constructor(
+    private readonly teamController: TeamController = teamControllerInstance,
+    private readonly authMiddleware: AuthMiddleware = authMiddlewareInstance,
+  ) {}
+
+  getRouter = (): Router => {
+    const router = Router();
+
+    router.get("/", this.teamController.listTeams);
+
+    router.post(
+      "/",
+      this.authMiddleware.requireAuth(),
+      this.teamController.createTeam,
+    );
+
+    router.get("/:teamId", this.teamController.getOneTeam);
+
+    router.patch("/:teamId", this.teamController.updateTeam);
+
+    router.delete("/:teamId", this.teamController.deleteTeam);
+
+    return router;
+  };
+}
+
+export const teamRouterInstance = new TeamRouter();
