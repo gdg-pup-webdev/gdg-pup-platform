8b88faf6eddde874b2f51c05be934903695ab69c
Randall Graida <carlosrandallgraida@gmail.com>
2026-01-30 15:35:50 +0800

    refactor(build): update dependencies

--- packages/nexus-api-contracts/src/routes/api/event-system/events/GET.ts
+++ packages/nexus-api-contracts/src/routes/api/event-system/events/GET.ts
@@ -1,7 +1,17 @@
 import { event } from "#models/eventSystem/index.js";
 import { SchemaFactory } from "#utils/schemaFactory.utils.js";
 
-export const query = SchemaFactory.Request.Paginated.query();
+import { z } from "zod";
+
+export const query = SchemaFactory.Request.Paginated.query().extend({
+  creator_id: z.string().optional(),
+  category: z.string().optional(),
+  venue: z.string().optional(),
+  start_date_gte: z.string().optional(),
+  start_date_lte: z.string().optional(),
+  end_date_gte: z.string().optional(),
+  end_date_lte: z.string().optional(),
+});
 
 export const response = {
   200: SchemaFactory.Response.paginated(event.row),
