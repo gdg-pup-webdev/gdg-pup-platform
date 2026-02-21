7d4f21c5507bd55799892a6f1d1908cacffa4421
Gerald Berongoy <geraldberongoy04@gmail.com>
2026-02-21 14:22:48 +0800

    fix(auth-system-web): enhance width size of the components

--- apps/nexus-web/src/app/signup/page.tsx
+++ apps/nexus-web/src/app/signup/page.tsx
@@ -5,7 +5,7 @@ import { SignUpForm } from "@/features/auth";
 export default function SignUpPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-purple-50 to-blue-50 px-4">
-      <div className="max-w-md w-full">
+      <div className="w-full">
         <div className="bg-white rounded-2xl shadow-xl p-8">
           <h1 className="text-3xl font-bold text-center mb-2 text-gray-800">
             Create Account
