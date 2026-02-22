0b749da39fd5c0d8a0629cbc3c2f9389b350721b
Rheen0 <s.rhandie@gmail.com>
2026-02-22 21:18:49 +0800

    refactor(button): implement gradient borders and multiple variants with updated styles and tokens

--- packages/spark-ui/src/components/button/Button.types.ts
+++ packages/spark-ui/src/components/button/Button.types.ts
@@ -1,8 +1,12 @@
 import type { ButtonVariants } from "./Button.styles";
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>, ButtonVariants {
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    ButtonVariants {
   asChild?: boolean;
+  iconLeft?: React.ReactNode | boolean;
+  iconRight?: React.ReactNode | boolean;
+  theme?: "light" | "dark";
 }
 
 export type { ButtonVariants };
