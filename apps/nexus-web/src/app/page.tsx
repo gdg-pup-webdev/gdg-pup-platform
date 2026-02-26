d840d6c0cfc08c5ad5c0fd82945a784e848522b2
Rheen0 <s.rhandie@gmail.com>
2026-02-26 21:37:46 +0800

    refactor(nexus-web): replace existing home layout according to feature components

--- apps/nexus-web/src/app/page.tsx
+++ apps/nexus-web/src/app/page.tsx
@@ -1,34 +1,12 @@
 "use client";
 
-import { Box, Button, Stack, Text } from "@packages/spark-ui";
+import { BelowHeroSection, HeroSection } from "@/features/home";
 
 export default function HomePage() {
   return (
-    <Stack align="center" justify="center" className="h-screen">
-      <Box className="relative z-10 text-center px-6 max-w-4xl">
-        <Text
-          variant="heading-1"
-          align="center"
-          gradient="white-blue"
-          weight="bold"
-          className="text-4xl md:text-5xl lg:text-6xl mb-6 leading-tight"
-        >
-          Bridging the gap between theory and practice.
-        </Text>
-        <Text
-          variant="body"
-          align="center"
-          weight="bold"
-          className="text-white/90 max-w-3xl mx-auto leading-relaxed"
-        >
-          GDG PUP helps student developers grow through real projects, events,
-          and mentorship connecting classroom learning to industry practice.
-        </Text>
-        {/* Insert an Icon here */}
-        <Button variant="default" size="lg" className="mt-8">
-          Spark your Journey
-        </Button>
-      </Box>
-    </Stack>
+    <>
+      <HeroSection />
+      <BelowHeroSection />
+    </>
   );
 }
