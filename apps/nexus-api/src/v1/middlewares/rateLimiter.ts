fa7f271cd70f4bcd8af1f3059e1009e67efa03dd
SauceCode01 <sauce.code.01@gmail.com>
2026-02-27 21:35:26 +0800

    feat(nexus-api): adjusted v1 rate limiter

--- apps/nexus-api/src/v1/middlewares/rateLimiter.ts
+++ apps/nexus-api/src/v1/middlewares/rateLimiter.ts
@@ -1,10 +1,9 @@
- 
 import { TooManyRequestError } from "@/v1/errors/HttpError";
 import { rateLimit } from "express-rate-limit";
 
 export const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per window (replaced 'max')
+  windowMs: 1 * 60 * 1000,
+  limit: 20,
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   // Skip express-rate-limit's proxy header validations.
