31cd388a07eda9c052bcf9d594e5596e9662d362
Erwin Daguinotas <daguinotaserwin5@gmail.com>
2026-03-19 01:38:37 +0800

    fix: fixed build errors

--- apps/nexus-admin-web/src/features/file-system/types.ts
+++ apps/nexus-admin-web/src/features/file-system/types.ts
@@ -1,5 +1,13 @@
-import { fileRecord, fileRecordInsertDTO, fileRecordUpdateDTO, folder, folderInsertDTO, folderUpdateDTO } from "@packages/nexus-api-contracts";
+import { contract, models } from "@packages/nexus-api-contracts";
 import { z } from "zod";
+
+const fileRecord = models.v1.fileSystem.file.fileRecord;
+const fileRecordInsertDTO = models.v1.fileSystem.file.fileRecordInsertDTO;
+const fileRecordUpdateDTO = models.v1.fileSystem.file.fileRecordUpdateDTO;
+
+const folder = models.v1.fileSystem.folder.folder;
+const folderInsertDTO = models.v1.fileSystem.folder.folderInsertDTO;
+const folderUpdateDTO = models.v1.fileSystem.folder.folderUpdateDTO;
 
 export type FileRecord = z.infer<typeof fileRecord>;
 export type FileRecordInsert = z.infer<typeof fileRecordInsertDTO>;
