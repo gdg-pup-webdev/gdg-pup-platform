2b14810f99b5e069a83013a410ad9aefa9a0eb21
r0undy <salesrhandie@gmail.com>
2026-03-22 01:23:20 +0800

    feat: add cosmos particles

--- apps/nexus-web/src/app/page.tsx
+++ apps/nexus-web/src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+
 import { WhoAreWeSection, HeroSection, WhatWeDoSection, WhatDrivesUsSection, ImpactSection, SparkStartsHereSection, HomeBackground } from "@/features/home";
+import { CosmosParticles } from "@/components/shared";
 
 export default function HomePage() {
   return (
@@ -10,13 +12,15 @@ export default function HomePage() {
       </div>
       <div className="relative z-10 bg-[#010B1D]">
         <HomeBackground />
-        <div className="relative z-10">
-          <WhoAreWeSection />
-          <WhatWeDoSection />
-          <WhatDrivesUsSection />
-          <ImpactSection />
-          <SparkStartsHereSection />
-        </div>
+        <CosmosParticles particleColors={["#ffffff"]} particleCount={135} className="min-h-screen">
+          <div className="relative z-10">
+            <WhoAreWeSection />
+            <WhatWeDoSection />
+            <WhatDrivesUsSection />
+            <ImpactSection />
+            <SparkStartsHereSection />
+          </div>
+        </CosmosParticles>
       </div>
     </main>
   );
