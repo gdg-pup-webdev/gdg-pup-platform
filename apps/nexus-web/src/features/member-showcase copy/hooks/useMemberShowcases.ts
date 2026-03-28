c1bc8d622f679db9465ecd35235ac0044000e788
SauceCode01 <sauce.code.01@gmail.com>
2026-03-28 16:24:56 +0800

    fix: fixed build errors

--- apps/nexus-web/src/features/member-showcase copy/hooks/useMemberShowcases.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getMemberShowcases } from "../api/getMemberShowcases";
-
-export function useMemberShowcases(pageNumber = 1, pageSize = 10) {
-  return useQuery({
-    queryKey: ["member-showcases", pageNumber, pageSize],
-    queryFn: () => getMemberShowcases(pageNumber, pageSize),
-  });
-}
