5d1507c950fa051395ddaaf6b2f2a0a05b047607
SauceCode01 <sauce.code.01@gmail.com>
2026-03-29 19:55:08 +0800

    fix: fixed build errors

--- apps/nexus-web/src/features/articles/index.ts
+++ apps/nexus-web/src/features/articles/index.ts
@@ -2,6 +2,5 @@ export * from "./hooks/useListArticle";
 export * from "./hooks/useCreateArticle";
 export * from "./hooks/useGetOneArticle";
 export * from "./hooks/useUpdateArticle";
-export * from "./hooks/useDeleteArticle";
-export * from "./components/ArticlesList";
+export * from "./hooks/useDeleteArticle"; 
 export * from "./types";
