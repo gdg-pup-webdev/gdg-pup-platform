eebf081dafd44e6f32bca162baa3ce7efe9d764d
SauceCode01 <sauce.code.01@gmail.com>
2026-04-26 17:39:58 +0800

    feat: fixed nfc card endpoiint always being called

--- apps/nexus-web/src/features/nfc-cards/hooks/useNfcCard.ts
+++ apps/nexus-web/src/features/nfc-cards/hooks/useNfcCard.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
-import { useCallEndpointWithToken as callEndpoint, useCallEndpointWithToken } from "@/hooks/useFetchWithToken";
+import { useCallEndpointWithToken } from "@/hooks/useFetchWithToken";
 import { contract } from "@packages/nexus-api-contracts";
 import { configs } from "@/lib/constants/configs";
 import { extractErrorMessage } from "@/lib/utils";
 import { useAuthContext } from "@/features/authentication/store/useAuthStore";
+import { getNfcCard } from "../api/getNfcCard";
 
 export const useNfcCard = (gdgId: string) => {
   const { token } = useAuthContext();
@@ -12,25 +13,8 @@ export const useNfcCard = (gdgId: string) => {
   return useQuery({
     queryKey: ["nfc-card", gdgId],
     queryFn: async () => {
-      console.log("Fetching NFC card with ID:", gdgId);
-      const res = await callEndpoint(
-        configs.nexusApiBaseUrl,
-        contract.api.v1.gdgmembers.gdgId.nfc_card.GET,
-        {
-          params: {
-            gdgId: gdgId,
-          },
-          token: token ?? undefined,
-        },
-      );
-
-      console.log("Fetched NFC card with ID:", gdgId);
-
-      if (res.status === 200) return res.body.data;
-
-      if (res.status === 404) return null;
-
-      throw new Error(extractErrorMessage(res.body));
+      const result = await getNfcCard(gdgId);
+      return result;
     }, 
   });
 };
